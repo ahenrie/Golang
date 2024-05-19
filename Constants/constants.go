b45c9992f86f9dd13e7f4ddae34c7f3e083ee0d7
Ahenrie <10914763@uvu.edu>
2024-05-18 21:21:35 -0700

    Update with comments

--- Constants/constants.go
+++ Constants/constants.go
@@ -11,10 +11,12 @@ func main() {
   fmt.Println(s)
 
   const n = 500000000
-
+  
+  //arbritrary precision 
   const d = 3e20 / n
   fmt.Println(d)
 
+  //explicit conversion
   fmt.Println(int64(d))
 
   fmt.Println(math.Sin(n))
