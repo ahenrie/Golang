8d36baa5ddd0ad57f0a8c18f606b78638ab671e0
Ahenrie <10914763@uvu.edu>
2024-09-26 16:08:42 -0700

    Simple generic examples added. go.mod and go.sum added

--- Generics/generics.go
+++ Generics/generics.go
@@ -2,8 +2,43 @@ package main
 
 import (
 	"fmt"
+
+	"golang.org/x/exp/constraints"
 )
 
+func AddInt(a int, b int) int {
+	return a + b
+}
+
+func AddFloat(a float64, b float64) float64 {
+	return a + b
+}
+
+/*
+Say we want to create a simple add function that can add whatever datatypes.
+How can we use generics to accomplish this?
+*/
+
+func AddGenerics[T int | float64](a T, b T) T {
+	return a + b
+}
+
+// There are lots of different types in Go so the approach is above is too verbose.
+// We can create our own datatype to interface with a generic function.
+type Num interface {
+	int | int8 | int16 | float32 | float64
+}
+
+func AddInteface[T Num](a T, b T) T {
+	return a + b
+}
+
+// This is still too verbose. We can use constraints.
+func Add[T constraints.Ordered](a T, b T) T {
+	return a + b
+}
+
 func main() {
-	fmt.Println("Hello world")
+	result := Add(1, 2.1)
+	fmt.Printf("result: %+v\n", result)
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module learning-go
+
+go 1.22.0
+
+require golang.org/x/exp v0.0.0-20240909161429-701f63a606c0 // indirect
