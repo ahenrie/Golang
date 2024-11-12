888fbc9d4ef4985e1780814aa42c7fa79da65e99
Ahenrie <10914763@uvu.edu>
2024-11-12 11:47:09 -0800

    Testing with interfaces

--- WithTests/Struct_Methods_Interfaces/go_test.go
+++ WithTests/Struct_Methods_Interfaces/go_test.go
@@ -24,6 +24,27 @@ func TestArea(t *testing.T) {
 	})
 }
 
+func TestAreaInterfaces(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+}
+
 func TestMethods(t *testing.T) {
 
 	t.Run("rectangle area", func(t *testing.T) {
