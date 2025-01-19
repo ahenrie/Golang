dc5db1e228b880789a4abfed1ca6427a7e385360
Ahenrie <10914763@uvu.edu>
2025-01-18 17:07:50 -0800

    Array manipulation

--- Arrays/arrays.go
+++ Arrays/arrays.go
@@ -1,28 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
 
 func main() {
-  //create an array that will hold 5 ints
-  var a [5]int
-  fmt.Println("emp:", a)
+	//create an array that will hold 5 ints
+	var a [5]int
+	fmt.Println("emp:", a)
 
-  //all indices will be written with a 0
+	//all indices will be written with a 0
 
-  //access 5th int 
-  a[4] = 100
-  fmt.Println("set:", a)
-  fmt.Println("get:", a[4])
+	//access 5th int
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
 
-  //print length of an array
-  fmt.Println("len:", len(a))
+	//print length of an array
+	fmt.Println("len:", len(a))
 
-  //declare and initialize in one line
-  b := [5]int{1, 2, 3, 4}
+	//declare and initialize in one line
+	b := [5]int{1, 2, 3, 4}
 
-  //same but use the compiler to count
-  b = [...]int{0, 0:100}
-  fmt.Println(b)
+	//same but use the compiler to count
+	//b = [...]int{0, 0:100}
+	fmt.Println(b)
 
+	// Construct a sorted count array
+	// I know it is only going to be of length 10 since my numbers range from 0 to 10
+	slice1 := []int{}
+	//rand.Seed(time.Now().UnixNano())
+	for i := 0; i < 100; i++ {
+		slice1 = append(slice1, rand.Intn(10))
+	}
+
+	countArr := make([]int, 11)
+	for _, num := range slice1 {
+		countArr[num]++
+	}
+	fmt.Println(countArr)
+
+	// Make the countArr a zigzag
+	n := len(countArr)
+	sort.Ints(countArr)
+	m := (n - 1) / 2
+	fmt.Println(countArr)
+	for i, j := m, n-1; i < j; i, j = i+1, j-1 {
+		countArr[i], countArr[j] = countArr[j], countArr[i]
+	}
+	fmt.Println(countArr)
 
 }
