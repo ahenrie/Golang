64d54cc468c36e4d3ce840bc7e4a521632418f12
Ahenrie <10914763@uvu.edu>
2025-02-09 11:12:02 -0700

    Starting a new book on Go, not organized well, but it will be here in the meantime

---
+++ Beginner to Professional/1: Variables and Operators/Value_vs_Pointer/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// variable declaration with var
+	var count1 *int
+	// variable with new()
+	count2 := new(int)
+	// Create a number to use its address
+	countTemp := 4
+	// Using &, create a pointer to an existing variable
+	count3 := &countTemp
+	// Create a pointer from some types without a temp variable
+	t := &time.Time{}
+	// Print each out
+	fmt.Printf("count1: %#v\n", count1)
+	fmt.Printf("count2: %#v\n", count2)
+	fmt.Printf("count3: %#v\n", count3)
+	fmt.Printf("time : %#v\n", t)
+}
