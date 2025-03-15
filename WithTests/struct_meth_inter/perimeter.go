a3c652a8c738f5d2c1211942020bd4a2445fa1ff
BASED <based@BSD.lan>
2025-03-15 09:08:56 -0600

    Adding in some methods to tests along with their tests

---
+++ WithTests/struct_meth_inter/perimeter.go
@@ -0,0 +1,12 @@
+package main
+
+
+//import "math"
+
+func Perimeter(width float64, height float64) float64 {
+  return 2 * (width + height)
+}
+
+func Area(width float64, height float64) float64 {
+  return width * height
+}
