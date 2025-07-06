c3e9104e4f54d6c7f46dd61b42453b3a014e965a
Ahenrie <10914763@uvu.edu>
2025-07-06 11:38:47 -0600

    Adding the root comd

--- csvSlicer/cmd/root.go
+++ csvSlicer/cmd/root.go
@@ -1,12 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+)
+
 var rootCmd = &cobra.Command{
-  Run: func(cmd *cobra.Command, args []string) {
-		
-	},
+    Use:   "csvSlicer",
+    Short: "csvSlcer trims large csv files",
+    Long: "csvSlcer trims large csv files",
+    Run: func(cmd *cobra.Command, args []string) {
+
+    },
 }
 
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(1)
-  }
+    if err := rootCmd.Execute(); err != nil {
+        fmt.Fprintf(os.Stderr, "Oops. An error while executing csvSlicer '%is'\n", err)
+        os.Exit(1)
+    }
 }
