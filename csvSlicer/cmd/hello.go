e5aeefcb302783576ed08aab91b958614cb63256
Ahenrie <10914763@uvu.edu>
2025-07-06 11:47:21 -0600

    Trying to get a test hello function to work

--- csvSlicer/cmd/hello.go
+++ csvSlicer/cmd/hello.go
@@ -1,8 +1,18 @@
 package cmd
 
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	"fmt"
+)
+
+var HelloCommand = &cobra.Command{
+	Use:	"say hello",
+	Run: func(cmd *cobra.Command) {
+		fmt.Println("Hello, there!")
+	},
+}
 
 func init() {
-	rootCmd.
+	rootCmd.HelloCommand()
 }
--- csvSlicer/main.go
+++ csvSlicer/main.go
@@ -1,5 +1,6 @@
 package main
 
+import "csvSlicer/cmd"
 
 func main() {
 	cmd.Execute
